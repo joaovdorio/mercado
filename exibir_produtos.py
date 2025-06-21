117bce072f9d8a6f94a8d9bea6c4a255dafcfa79
joaovdorio <joaovitor.dorio@gmail.com>
2025-06-21 13:00:53 -0300

    changed file name to be more assertive with code

--- exibir_produtos.py
+++ exibir_produtos.py
@@ -5,5 +5,5 @@ def exibir_produtos():
     for indice, produto in enumerate(base.listagem, start=1):
         print(f"{indice}. {produto.nome} \n SKU: {produto.sku} \n Código de barras: {produto.cod_barras} \n Peso: {produto.peso}kg \n")
         markup = produto.valor_final / produto.valor_compra if produto.valor_compra != 0 else 0
-        print(f"Valor adquirido: R${produto.valor_compra} \n Valor de venda: R${produto.valor_final} \n Markup: {markup:.2f}x \n")
+        print(f"Valor adquirido: R${produto.valor_compra} \n Valor para consumidor: R${produto.valor_final} \n Markup: {markup:.2f}x \n")
         print(f"Quantidade em estoque: {produto.quantidade}")
